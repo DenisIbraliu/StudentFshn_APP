792ebd23da1428f900d88d8cf4a532aae0c420f5
eva nako <evanako2000@outlook.com>
2021-04-21 18:32:27 +0200

    add sign in

---
+++ StudentFshn/Student/SignInStudent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace StudentFshn.Student
+{
+    class SignInStudent
+    {
+        public int Id { get; set; }
+        public string Emri { get; set; }
+        public string Mbiemri { get; set; }
+        public string Email { get; set; }
+    }
+}
